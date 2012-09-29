ea9f6cd1b3a5ece74e83ce3339c2e6b62597a98b
Ozan Sener <ozan@ozansener.com>
2012-09-29 10:39:47 +0300

    Remove slime and clojure bits I'm not using

--- init-clojure.el
+++ init-clojure.el
@@ -1,42 +1,4 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Slime with Clojure
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Use technomancy's bag of fancy clojure/slime tricks
-(eval-after-load 'slime
-  '(progn
-     (require 'durendal)
-     (durendal-enable t)
-     (durendal-disable-slime-repl-font-lock)))
-
-(defun slime-clojure-repl-setup ()
-  "Some REPL setup additional to that in durendal."
-  (when (string-equal (slime-lisp-implementation-name) "clojure")
-    (when (slime-inferior-process)
-      (message "Setting up repl for clojure")
-      (slime-redirect-inferior-output))
-
-    (set-syntax-table clojure-mode-syntax-table)
-    (setq lisp-indent-function 'clojure-indent-function)
-    (let (font-lock-mode)
-      (clojure-mode-font-lock-setup))))
-
-(add-hook 'slime-repl-mode-hook 'slime-clojure-repl-setup)
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; nrepl with Clojure
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
-(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
-(eval-after-load "auto-complete"
-  '(add-to-list 'ac-modes 'nrepl-mode))
-
-(add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
-(add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Misc clojure tweaks
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -65,9 +27,6 @@
     (font-lock-add-keywords nil x)))
 
 (add-hook 'clojure-mode-hook 'tweak-clojure-syntax)
-
-
-
 
 (eval-after-load 'gist
   '(add-to-list 'gist-supported-modes-alist '(clojure-mode . ".clj")))
