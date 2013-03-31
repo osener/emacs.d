f298af7d219a97d3638d6179c1c31ab5a8154e69
Ozan Sener <ozan@ozansener.com>
2013-03-31 22:49:53 +0300

    Remove incompatible binding for multiple cursors

--- init-editing-utils.el
+++ init-editing-utils.el
@@ -125,7 +125,6 @@
 ;; multiple-cursors
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-+") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 ;; From active region to multiple cursors:
 (global-set-key (kbd "C-c c r") 'set-rectangular-region-anchor)
