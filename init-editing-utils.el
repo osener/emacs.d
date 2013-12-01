3783c943df3440239efa7761fcd2d2a414fe3689
Ozan Sener <ozan@ozansener.com>
2013-12-01 11:28:15 +0200

    Replace autopair with smartparens

--- init-editing-utils.el
+++ init-editing-utils.el
@@ -97,18 +97,6 @@
 (require-package 'expand-region)
 (global-set-key (kbd "C-=") 'er/expand-region)
 
-;;----------------------------------------------------------------------------
-;; Autopair quotes and parentheses
-;;----------------------------------------------------------------------------
-(require-package 'autopair)
-(require 'autopair)
-(setq autopair-autowrap t)
-(autopair-global-mode t)
-
-(defun inhibit-autopair ()
-  "Prevent autopair from enabling in the current buffer."
-  (setq autopair-dont-activate t)
-  (autopair-mode -1))
 
 ;;----------------------------------------------------------------------------
 ;; Don't disable case-change functions
