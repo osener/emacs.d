e96251b9c6eec4f0380b0044f76716830ed17487
Ozan Sener <ozan@ozansener.com>
2014-08-15 20:50:25 +0300

    Disable ledger

--- init.el
+++ init.el
@@ -97,7 +97,7 @@
 (require 'init-misc)
 
 (require 'init-dash)
-(require 'init-ledger)
+;; (require 'init-ledger)
 ;; Extra packages which don't require any configuration
 
 (require-package 'gnuplot)
