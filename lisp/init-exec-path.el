8086dd03be632af9563921e78d425ef7cd668d7c
Ozan Sener <ozan@ozansener.com>
2014-12-01 18:16:57 +0200

    Hardcode exec-path variables to omit PATH

--- lisp/init-exec-path.el
+++ lisp/init-exec-path.el
@@ -1,9 +1,9 @@
 (require-package 'exec-path-from-shell)
 
 (after-load 'exec-path-from-shell
-  (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE"))
-    (add-to-list 'exec-path-from-shell-variables var)))
-
+  (setq exec-path-from-shell-variables
+        '("LC_CTYPE" "LANG" "GPG_AGENT_INFO" "SSH_AGENT_PID"
+          "SSH_AUTH_SOCK" "MANPATH")))
 
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
